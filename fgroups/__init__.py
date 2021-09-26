f1e1960254d540feefb5be430e4960c980030d84
Cristobal Torres <cristobal_javier@hotmail.com>
2021-09-26 19:02:49 -0300

    Added README

--- fgroups/__init__.py
+++ fgroups/__init__.py
@@ -6,5 +6,5 @@ pyexample.
 An example python library.
 """
 
-__version__ = "0.0.1"
-__author__ = 'Cris Torres'
+__version__ = "0.0.2"
+__author__ = "Cris Torres"
--- setup.py
+++ setup.py
@@ -1,10 +1,23 @@
+#!/usr/bin/env python3
+#
+# Copyright (C) 2021 Cris Torres <cristobal_javier@hotmail.com>
+# MIT License
+
 import setuptools
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 
 setuptools.setup(
     name="finite-groups",
-    version="0.0.1",    
+    version="0.0.2",
     description="Finite Groups Computations",
-    url="https://github.com/CrisLeaf/finite-groups",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    project_urls={
+        "GitHub": "https://github.com/CrisLeaf/finite-groups",
+    },
     author="Cris Torres",
     author_email="cristobal_javier@hotmail.com",
     license="MIT License",
@@ -16,7 +29,7 @@ setuptools.setup(
         "Development Status :: 1 - Planning",
         "Intended Audience :: Science/Research",
         "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License",  
+        "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3.9",
         "Topic :: Scientific/Engineering",
         "Operating System :: POSIX",
